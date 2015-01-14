78542d20cafbcfaaa702c751fa37138927beb66a
Muneeb Samuels <muneeb@actaeon.co.za>
2015-01-14 16:13:02 +0200

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Muneeb Samuels on 2015/01/14.
+//  Copyright (c) 2015 Codebus.io (Pty) Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
